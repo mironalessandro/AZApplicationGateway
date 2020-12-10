496a7689ad96507d72c483f8f30ca602c6fc5830
Alessandro Miron <54042200+mironalessandro@users.noreply.github.com>
2020-12-10 11:55:03 +0100

    Powershell Script

---
+++ Azure - Application Gateway.ps1
@@ -0,0 +1,11 @@
+﻿Connect-AzAccount
+
+# Get Azure Application Gateway
+$appgw=Get-AzApplicationGateway -Name DIS-TEST -ResourceGroupName RG-DBG-EUROPE
+
+# Stop the Azure Application Gateway
+Stop-AzApplicationGateway -ApplicationGateway $appgw
+ 
+# Start the Azure Application Gateway (optional)
+#Start-AzApplicationGateway -ApplicationGateway $appgw
+
